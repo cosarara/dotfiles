3c76ba04cc80d225443685e961db92e983c5fbdf
Jaume Delclòs Coll <cosarara97@gmail.com>
2016-09-14 16:47:17 +0200

    autostart scripts

---
+++ config/autostart.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+# Configure PATH so that we can access our scripts below
+#PATH=$PATH:~/.cabal/bin:~/.xmonad/bin
+
+# Configure X
+#xsetroot -cursor_name left_ptr &
+#xrdb -merge ~/.Xdefaults &
+
+# Start a window compositor. This ensures Google Chrome paints correctly.
+#xcompmgr -n &
+#if [[ "$DISPLAY" == ":0" ]]; then
+#	killall compton || true
+#	compton --config ~/.config/compton &
+#fi
+#nitrogen --restore &
+#feh --randomize --bg-fill --no-xinerama ~/Pictures/wallpaper
+#randwallpaper
+
+#trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 4 --transparent true --alpha 17 --tint 0x34322E --height 16 --distance 4 --margin 160
+
+killall compton || true
+compton --config ~/.config/compton &
+
+~/.config/autostart_once.sh
+
+# Start the system tray
+#tray &
+
+# Start two terminals
+#gnome-terminal &
+#gnome-terminal &
+#
+## Start a browser
+#google-chrome &
+
+# Start screensaver
+# Kill gnome-screensaver if started by default
+#killall gnome-screensaver &
+#xscreensaver -no-splash &
+
+# Start sound server
+#pulseaudio --start &
+
+#exec ck-launch-session dbus-launch --sh-syntax --exit-with-session xmonad
+
+wait
---
+++ config/autostart_once.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Uses X resources to only run once per X11
+
+
+PROP=autostart.wasrun
+HASPROP=$(xrdb -query|grep $PROP)
+
+if [ "$HASPROP" != "" ]
+then
+  exit 0;
+fi
+
+echo "$PROP: on" | xrdb -merge
+
+killall nm-applet || true
+XDG_CONFIG_HOME=$HOME/.config/bspwm/theme_overrides nm-applet &
+
+mpd
+
+killall mpdas; mpdas &
+
+randwallpaper
+
