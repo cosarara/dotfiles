47815149aaf70d0758a169693e374a51fff17b76
Jaume Delclòs Coll <cosarara97@gmail.com>
2019-09-12 20:53:35 +0200

    otb terminus

--- config/awesome/mytheme/theme.lua
+++ config/awesome/mytheme/theme.lua
@@ -13,8 +13,8 @@ theme.dir = os.getenv("HOME") .. "/.config/awesome/mytheme"
 -- }}}
 
 -- {{{ Styles
-theme.font      = "xos4 Terminus 8"
-theme.icon_font      = "terminusicons2mono 8"
+theme.font      = "Terminus 8"
+theme.icon_font      = "Terminusicons2Mono 8"
 
 --[[ {{{ Colors
 theme.fg_normal  = "#DCDCCC"
